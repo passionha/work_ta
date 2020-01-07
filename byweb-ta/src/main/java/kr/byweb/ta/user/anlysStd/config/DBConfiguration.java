71bde97773b16fc963866987b66fa70bcfa3ba28
BYWEB-008 <BYWEB-008@BYWEB-008>
2020-01-07 17:48:13 +0900

    jsp수정중

--- byweb-ta/src/main/java/kr/byweb/ta/user/anlysStd/config/DBConfiguration.java
+++ byweb-ta/src/main/java/kr/byweb/ta/user/anlysStd/config/DBConfiguration.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -45,12 +46,13 @@ public class DBConfiguration {
 	 */
 	@Autowired
 	private ApplicationContext applicationContext;
-
+	
+	
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
-		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis/mapper/**/*.xml"));
+		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mybatis/mapper/**/*.xml"));
 		return sqlSessionFactoryBean.getObject();
 	}
 
--- byweb-ta/src/main/java/kr/byweb/ta/user/anlysStd/controller/AnlysStdController.java
+++ byweb-ta/src/main/java/kr/byweb/ta/user/anlysStd/controller/AnlysStdController.java
@@ -1,19 +1,22 @@
 package kr.byweb.ta.user.anlysStd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.byweb.ta.user.anlysStd.service.AnlysStdService;
 
-@RestController
+@Controller
 public class AnlysStdController {
 	
 	@Autowired
 	private AnlysStdService anlysStdService;
 	
-	@GetMapping(path = "/helloWorld")
-	public String helloWorld() {
+//	@GetMapping(path = "/selectTime")
+	@RequestMapping("/selectTime")
+	public @ResponseBody String selectTime() {
 		
 		String strTime = "";
 		try {
@@ -22,5 +25,19 @@ public class AnlysStdController {
 			e.printStackTrace();
 		}
 		return strTime;
+	}
+	
+	@RequestMapping("/helloWorld")
+	public String helloWorld(Model model) {
+		
+		model.addAttribute("name", "홍길동");
+		
+		return "anlysStdList";
+	}
+	
+	@RequestMapping("/")
+	public String anlysStdMain() {
+		
+		return "anlysStd/anlysStdMain";
 	}
 }
